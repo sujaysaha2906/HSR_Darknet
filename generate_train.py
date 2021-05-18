cf7c9b705c82b12f2a9331e07e8b30ab8353b4ab
sujaysaha2906 <sujaysaha2906@github.com>
2021-05-18 10:37:08 +0600

    Update generate_train.py

--- generate_train.py
+++ generate_train.py
@@ -1,10 +1,10 @@
 import os
 
 image_files = []
-os.chdir(os.path.join("data", "obj"))
+os.chdir(os.path.join("data", "train"))
 for filename in os.listdir(os.getcwd()):
     if filename.endswith(".JPG") or filename.endswith(".jpg"):
-        image_files.append("data/obj/" + filename)
+        image_files.append("data/train/" + filename)
 os.chdir("..")
 with open("train.txt", "w") as outfile:
     for image in image_files:
